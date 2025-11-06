f7fe09f5758269e9812850d0576af27289848952
Xy-yang666 <xiaoyu.xy01.yang@gmail.com>
2025-11-06 22:42:24 +0100

    test chunk,compact,fill

--- lodash/xiaoyuy2906-lodash.js
+++ lodash/xiaoyuy2906-lodash.js
@@ -1,23 +1,47 @@
 var xiaoyuy2906 = {
     compact: function (array) {
-        return array.filter(item => item)
+        /* return array.filter(item => item) */
+
+        var result = []
+        for (var item of array) {
+            if (item) {
+                result.push(item)
+            }
+        }
+        return result   //不用slice实现
     },
 
     chunk: function (array, size) {
         var result = []
-        var i = 0
-        var remainder = array.length % size
-        while (i + size <= array.length - remainder) {
-            result.push(array.slice(i, i + size))
-            i += size
+        var s = Math.floor(size)
+        if (s < 1) {
+            return result
+        } else if (array.length <= s) {
+            result.push(array)
+            return result
+        } else {
+            // for (var i = 0; i < array.length; i += s) {
+            //     result.push(array.slice(i, i + s))
+            // }
+            var currentArr = []
+            for (var i = 0; i < array.length; i++) {
+                if ((i + 1) % s == 0) {
+                    currentArr.push(array[i])
+                    result.push(currentArr)
+                    currentArr = []
+                } else {
+                    currentArr.push(array[i])
+                }
+            }
+            if (currentArr.length > 0) {
+                result.push(currentArr)
+            }
+            return result   //不用slice实现
         }
-        if (i < array.length) {
-            result.push(array.slice(array.length - remainder))
-        }
-        return result
     },
 
     fill: function (array, value, start = 0, end = array.length) {
-        return array.slice(0, start).concat(array.slice(start, end).map(it => value)).concat(array.slice(end))
+        var myArr = Array.from(array)
+        return myArr.slice(0, start).concat(myArr.slice(start, end).map(it => value)).concat(myArr.slice(end))
     }
 }
